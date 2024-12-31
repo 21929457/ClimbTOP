caab93e3e862681fdd6aaacdf1c8cb845c25a2fa
21929457 <apple855005@gmail.com>
2024-12-31 09:22:07 +0900

    data 내용변경

--- src/components/Store.js
+++ src/components/Store.js
@@ -1,35 +1,45 @@
-// import $ from 'jquery'
-// import axios from 'axios'
+import $ from 'jquery'
+import axios from 'axios'
 
 export default function Store() {
-    // let items = $('.items')
+    let buyBtn = $('.info')
+    let closeBtn = $('.closeBtn')
+    let disc = $('.disc-wrap')
+    let items = $('.items')
 
-    // axios.get('../data/item.json').then(function(data){
-    //     data.forEach(function(e , i) {
-    //         items.append(`
-    //             <div className='item' style={{backgroundImage : url("${e.url}")}}>
-    //                 <p className='name'>${e.name}</p>
-    //                 <p className='disc'>${e.disc}</p>
-    //                 <p className='cost'><span>${e.cost}</span>G</p>
-    //                 <button type='button'>구매</button>
-    //             </div>
-    //         `)
-    //     })
-    // }).catch(function(){
+    buyBtn.on('click' , function(){
+        disc.addClass('show')
+    })
+    closeBtn.on('click' , function(){
+        disc.removeClass('show')
+    })
 
-    // })
+    axios.get('https://raw.githubusercontent.com/21929457/ClimbTOP/refs/heads/master/src/data/item.json').then(function(data){
+        data.data.forEach(function(e){
+            items.append(`
+                <div className='item'>
+                 <img alt="item_1"/>
+                 <div className="btn-wrap">
+                     <button type='button' className="info">정보</button>
+                     <span></span>
+                     <button type='button' className="buy">구매</button>
+                 </div>
+                 <div className='disc-wrap'>
+                     <button type='button' className='closeBtn'></button>
+                     <p className='name'>${e.name}</p>
+                     <p className='cost'><span>${e.cost}</span>G</p>
+                     <span className='line'></span>
+                     <p className='disc'>${e.disc}</p>
+                 </div>
+             </div>
+            `)
+        })
+    }).catch(function(){
+
+    })
     return(
         <div className="contents-view store-contents ">
-            <div className='items'>
-                <div className='item'>
-                    <p className='name'>위자드 웜</p>
-                    <p className='cost'><span>50</span>G</p>
-                    <div className="btn-wrap">
-                        <button type='button' className="info">정보</button>
-                        <button type='button' className="buy">구매</button>
-                    </div>
-                </div>
-            </div>
+            <div className='items'></div>
         </div>
     )
 }
