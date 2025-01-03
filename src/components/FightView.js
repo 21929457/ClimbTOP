232f960a7d431f3c4a5d0356d053f44cb74ed0d7
21929457 <apple855005@gmail.com>
2025-01-03 11:54:37 +0900

    일부 수정

--- src/components/FightView.js
+++ src/components/FightView.js
@@ -1,14 +1,14 @@
 import {useState} from 'react'
 
 export default function FightView() {
-    let [level , setLevel] = useState(1)
+    let [floor , setFloor] = useState(1)
     return(
         <div className='fight-view'>
             <div className='monster-hp-wrap px-20'>
                 <div className='monster-hp bg-red-500'></div>
                 <div className='monster-hp-text'><span className='current text-sm font-semibold'>100</span> / <span className='total text-sm font-semibold'>100</span></div>
             </div>
-            <p className="level">Lv.<span>{level}</span></p>
+            <p className="floor"><span>{floor} 층</span></p>
        </div>
     )
 }
